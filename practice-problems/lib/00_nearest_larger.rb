8ab889477e6d8964bee68cd5a419a77d64837003
willardgmoore <will@thefixitgeek.com>
2015-05-27 12:58:57 -0700

    Progress
    No idea where I left off on it,

--- practice-problems/lib/00_nearest_larger.rb
+++ practice-problems/lib/00_nearest_larger.rb
@@ -1,4 +1,21 @@
 def nearest_larger(arr, idx)
+	diff = 1
+	while true
+		left = idx - diff
+		right = idx + diff
 
-return idx2
+		if arr[idx] < arr[left]
+			return left
+		elsif arr[idx] < arr[right]
+			return right
+		elsif (left < 0) && (right >= arr.length)
+      		return nil
+		end
+
+
+puts "diff = #{diff}"
+puts "right = #{right}"
+		diff += 1
+	end
+
 end
