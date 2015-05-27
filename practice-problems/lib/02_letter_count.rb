434e0f397e278b21fb730d8219253624425fe012
willardgmoore <will@thefixitgeek.com>
2015-05-27 13:14:46 -0700

    Progress
    Thinking…

--- practice-problems/lib/02_letter_count.rb
+++ practice-problems/lib/02_letter_count.rb
@@ -1,2 +1,18 @@
 def letter_count(str)
+  result = {}
+
+  idx = 0
+  while idx < str.length
+
+# check if in hash
+# if true, update hash
+# if false
+# add to hash
+
+    idx += 1
+  end
+
+  return result
 end
+
+
