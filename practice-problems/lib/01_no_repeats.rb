7f0dce23b83adeba4d6e0d39c23977ac95ab2064
willardgmoore <will@thefixitgeek.com>
2015-05-27 12:58:16 -0700

    Solved
    Crushed it!

--- practice-problems/lib/01_no_repeats.rb
+++ practice-problems/lib/01_no_repeats.rb
@@ -1,2 +1,38 @@
+def no_repeat?(year)
+  year = year.to_s
+
+  idx = 0
+  while idx < year.length
+
+    idx1 = idx + 1
+    while idx1 < year.length
+	
+      if year[idx] == year[idx1]
+        return false
+      end
+
+      idx1 += 1
+    end
+
+    idx += 1
+  end
+
+  return true
+end
+
+
+
 def no_repeats(year_start, year_end)
+  result = []
+  year = year_start
+
+  while year <= year_end 
+
+    if no_repeat?(year) == true
+      result << year
+    end
+
+    year += 1
+  end
+  return result
 end
