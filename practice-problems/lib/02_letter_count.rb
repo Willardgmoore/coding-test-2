59f05cbdd636101c8d6ca9770a507f927d799963
willardgmoore <will@thefixitgeek.com>
2015-05-27 21:38:04 -0700

    Progress
    Still working on it.

--- practice-problems/lib/02_letter_count.rb
+++ practice-problems/lib/02_letter_count.rb
@@ -1,18 +1,36 @@
-def letter_count(str)
-  result = {}
-
-  idx = 0
-  while idx < str.length
-
-# check if in hash
-# if true, update hash
-# if false
-# add to hash
-
-    idx += 1
+def in_hash?(str)
+  if true
+    return true
+  else
+    return false
   end
 
-  return result
-end
 
+  def letter_count(str)
+    result = {}
+
+    idx = 0
+    while idx < str.length
+    
+      if in_hash?(str) == true
+        #Update Hash
+      else in_hash?(str) == false
+        result << "false"#Add hash
+ #     else 
+#        puts "Error!"
+      end
+    
+
+      # check if in hash
+      # if true, update hash
+      # if false
+      # add to hash
+
+      idx += 1
+    end
+
+    return result
+  end
+
+end
 
