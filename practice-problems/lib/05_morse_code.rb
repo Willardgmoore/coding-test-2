92aa0717aa3a205e705e155b42007d25f189f520
willardgmoore <will@thefixitgeek.com>
2015-05-29 11:51:24 -0700

    Progress
    about 1/2 done. Got the first letters

--- practice-problems/lib/05_morse_code.rb
+++ practice-problems/lib/05_morse_code.rb
@@ -1,2 +1,60 @@
+# Build a function, `morse_encode(str)` that takes in a string (no
+# numbers or punctuation) and outputs the morse code for it. See
+# http://en.wikipedia.org/wiki/Morse_code. Put two spaces between
+# words and one space between letters.
+#
+# You'll have to type in morse code: I'd use a hash to map letters to
+# codes. Don't worry about numbers.
+#
+# I wrote a helper method `morse_encode_word(word)` that handled a
+# single word.
+#
+# Difficulty: 2/5
+
+
+
 def morse_encode(str)
+
+morse_code = {
+  "a" => ".-",
+  "b" => "-...",
+  "c" => "-.-.",
+  "d" => "-..",
+  "e" => ".",
+  "f" => "..-.",
+  "g" => "--.",
+  "h" => "....",
+  "i" => "..",
+  "j" => ".---",
+  "k" => "-.-",
+  "l" => ".-..",
+  "m" => "--",
+  "n" => "-.",
+  "o" => "---",
+  "p" => ".--.",
+  "q" => "--.-",
+  "r" => ".-.",
+  "s" => "...",
+  "t" => "-",
+  "u" => "..-",
+  "v" => "...-",
+  "w" => ".--",
+  "x" => "-..-",
+  "y" => "-.--",
+  "z" => "--..",
+  " " => " " # This is to keep the extra space between words, 
+  ## We will put a space between everything else anyways.
+}
+
+  idx = 0
+  result = ""
+  words = str.split ""
+  puts " words = #{words}, morse = #{morse_code[words[idx]]}"		###############
+
+
+
+  result = morse_code[words]
+  puts "result #{result}"		################
+
+  return result
 end
